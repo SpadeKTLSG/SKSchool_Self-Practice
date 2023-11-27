58a19349bb236ad2b2738b4e7ba49e61bc1c2203
SpadeKtlsg <spadekxcwxtlsg@gmail.com>
2023-11-27 14:38:43 +0800

    吼吼吼!

--- src/main/java/com/tlsg/skxy/controller/ClazzController.java
+++ src/main/java/com/tlsg/skxy/controller/ClazzController.java
@@ -21,30 +21,36 @@ public class ClazzController {
     @Autowired
     private ClazzService clazzService;
 
-    //查全部班级信息
+    //?查全部班级信息
+    //NOTE : 哎呀前面查全部和这个的接口名称不一样, Service的命名也有点乱呢...
     @Operation(summary = "查询所有班级", description = "查询所有班级信息", parameters = {})
     @GetMapping("/getClazzs")
     public Result<Object> getClazzs() {
         List<Clazz> clazzes = clazzService.getClazzs();
         return Result.ok(clazzes);
     }
+    //http://localhost:8080/sms/clazzController/getClazzs
 
-    //删除单个和多个班级
-    @Operation(summary = "删除班级", description = "删除单个和多个班级", parameters = @Parameter(name = "ids", description = "要删除的多个班级的ID的JSON数组", required = true))
+    //?删除单个和多个班级
+    //
+    @Operation(summary = "删除班级", description = "删除单个和多个班级", parameters = {
+            @Parameter(name = "ids", description = "要删除的多个班级的ID的JSON数组", required = true)})
     @DeleteMapping("/deleteClazz")
     public Result<Object> deleteClazz(@RequestBody List<Integer> ids) {
         clazzService.removeByIds(ids);
         return Result.ok();
     }
+    //http://localhost:8080/sms/clazzController/deleteClazz?ids=1
 
 
     //增加或者修改班级信息
-    @Operation(summary = "增加或者修改班级", description = "增加或者修改班级信息", parameters = @Parameter(name = "clazz", description = "JSON格式的班级信息", required = true))
+    @Operation(summary = "增加或者修改班级", description = "增加或者修改班级信息", parameters = {
+            @Parameter(name = "clazz", description = "JSON格式的班级信息", required = true)})
     @PostMapping("/saveOrUpdateClazz")
     public Result<Object> saveOrUpdateClazz(@RequestBody Clazz clazz) {
         clazzService.saveOrUpdate(clazz);
         return Result.ok();
-    }
+    }//http://localhost:8080/sms/clazzController/saveOrUpdateClazz
 
 
     //分页带条件查询班级信息
